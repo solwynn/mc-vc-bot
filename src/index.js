1121f7d4a211e803112b3431f3d7fa7a080632da
willowsnow <willow.snow14@gmail.com>
2019-10-05 17:54:14 -0500

    Completely refactor everything

--- src/index.js
+++ src/index.js
@@ -1,55 +1,52 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-const config = require(__dirname + '/../config.json');
-const fs = require('fs');
-let channels = require(__dirname + '/../channels.json');
+(async () => {
+    const Discord = require('discord.js');
+    const bot = new Discord.Client();
+    const config = require(__dirname + '/../config.json');
+    const sqlite = require('sqlite');
+    const db = await sqlite.open(__dirname + '/../db/channels.db');
 
-function updateChannels() {
-    fs.writeFileSync(__dirname + '/../channels.json', JSON.stringify(channels));
-}
+    async function purgeAll() {
+        const channels = await db.all('SELECT text_id FROM channels WHERE set_to_purge = 1');
 
-function purge() {
-    let isPurged = 0;
+        for (const channel of channels) {
+            await purge(bot.channels.get(channel.text_id));
+        }
+    }
 
-    Object.keys(channels).forEach(async (id) => {
-        if (channels[id].setToPurge) {
-            const textChan = bot.channels.get(channels[id].textChannel);
-            while (!isPurged) {
-                await textChan.fetchMessages({ limit: 100 }).then(async (messages) => {
-                    if (messages.array().length > 0) {
-                        await textChan.bulkDelete(messages);
-                    } else {
-                        isPurged = 1;
-                    }
-                });
+    async function purge(channel) {
+        while (true) {
+            const messages = await channel.fetchMessages({ limit: 100 });
+            if (messages.array().length > 0) {
+                await channel.bulkDelete(messages);
+            } else {
+                db.run('UPDATE channels SET set_to_purge = 0 WHERE text_id = ?', [channel.id]);
+                break;
             }
-            channels[id].setToPurge = false;
-            updateChannels();
         }
+    }
+
+    bot.on('voiceStateUpdate', async (oldMember, newMember) => {
+        const channels = await db.all('SELECT voice_id, text_id FROM channels');
+
+        // Dealing with a purge channel
+        if (channels.some((v) => v.voice_id == oldMember.voiceChannelID)) {
+            const channel = bot.channels.get(oldMember.voiceChannelID);
+            if (!channel.members.length) {
+                db.run('UPDATE channels SET set_to_purge = 1 WHERE voice_id = ?', [oldMember.voiceChannelID]);
+            }
+        }
+
+        // Dealing with a purge channel
+        if (channels.some((v) => v.voice_id == newMember.voiceChannelID)) {
+            db.run('UPDATE channels SET set_to_purge = 0 WHERE voice_id = ?', [newMember.voiceChannelID]);
+        }
+
     });
-}
 
-bot.on('voiceStateUpdate', (oldMember, newMember) => {
-    
-    // Dealing with a purge channel
-    if (channels[oldMember.voiceChannelID]) {
-        let channel = bot.channels.get(oldMember.voiceChannelID);
-        if (!channel.members.length) {
-            channels[oldMember.voiceChannelID].setToPurge = true;
-            updateChannels();
-        }
-    }
-
-    // Dealing with a purge channel
-    if (channels[newMember.voiceChannelID]) {
-        channels[newMember.voiceChannelID].setToPurge = false;
-        updateChannels();
-    }
-
-});
-
-bot.on('ready', () => {
-    setInterval(purge, 1800000);
-});
-
-bot.login(config.token);
+    bot.once('ready', async () => {
+        db.run('UPDATE channels SET set_to_purge = 0');
+        setInterval(purgeAll, 5000);
+    });
+        
+    bot.login(config.token);
+})();
